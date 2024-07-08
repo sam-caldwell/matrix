360b2cf83ccff130c26c175612aebc24010bd4ea
Sam Caldwell <github@samcaldwell.net>
2024-07-08 18:00:37 -0500

    cleanup NewMatrix()

--- NewMatrix.go
+++ NewMatrix.go
@@ -1,13 +1,19 @@
 package matrix
 
 import (
+	"fmt"
+	"github.com/sam-caldwell/errors"
 	"github.com/sam-caldwell/types"
 )
 
-// NewMatrix - Generate a new generic matrix of row x col using type T.
-func NewMatrix[T types.Number](rows, cols uint) *Matrix[T] {
+// NewMatrix - Generate a new generic matrix of (row X col) using type T.
+func NewMatrix[T types.Number](rows, cols uint) (result *Matrix[T], err error) {
 
 	var m Matrix[T]
+
+	if rows == 0 || cols == 0 {
+		return nil, fmt.Errorf(errors.BoundsCheckError)
+	}
 
 	m.data = make([][]T, rows)
 
@@ -17,6 +23,6 @@ func NewMatrix[T types.Number](rows, cols uint) *Matrix[T] {
 
 	}
 
-	return &m
+	return &m, nil
 
 }
