014bdd163e995716b4df339429d8a87a3e220065
Sam Caldwell <github@samcaldwell.net>
2024-07-07 22:34:22 -0500

    add Cuda version of NewMatrix()

--- Cuda.NewMatrix.go
+++ Cuda.NewMatrix.go
@@ -6,8 +6,6 @@ package matrix
 import "gocv.io/x/gocv"
 
 // NewMatrix creates a new matrix with the given dimensions.
-//
-// (c) 2024 Sam Caldwell.  See License.txt
 func NewMatrix(rows, cols int) *Matrix {
 	data := gocv.NewMatWithSize(rows, cols, gocv.MatTypeCV64F)
 	return &Matrix{
