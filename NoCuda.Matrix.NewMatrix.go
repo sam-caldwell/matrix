7cd7f64053cf08769e4775c1a00518aa319f1570
Sam Caldwell <github@samcaldwell.net>
2024-07-07 22:38:38 -0500

    add NewSimple2d(), and cuda/no-cuda NewMatrix()

--- NoCuda.Matrix.NewMatrix.go
+++ NoCuda.Matrix.NewMatrix.go
@@ -1,15 +1,13 @@
-//go:build cuda
-// +build cuda
+//go:build !cuda
+// +build !cuda
 
 package matrix
-
-import "gocv.io/x/gocv"
 
 // NewMatrix creates a new matrix with the given dimensions.
 func NewMatrix(rows, cols int) *Matrix {
 
 	return &Matrix{
-		data: gocv.NewMatWithSize(rows, cols, gocv.MatTypeCV64F),
+		data: NewSimple2d(rows, cols),
 	}
 
 }
