95a79f351f91dd765a656a99212b462384afeb71
Sam Caldwell <github@samcaldwell.net>
2024-07-08 18:11:25 -0500

    extend NewMatrix() test

--- NewMatrix_test.go
+++ NewMatrix_test.go
@@ -7,10 +7,30 @@ import (
 
 func Test_NewMatrix(t *testing.T) {
 
-	t.Run("happy path: simple creation of a matrix", func(t *testing.T) {
+	t.Run("happy path: simple creation of a 5x2 matrix", func(t *testing.T) {
 		const (
 			rowSize = 5
 			colSize = 2
+		)
+
+		m, err := NewMatrix[int](rowSize, colSize)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if actual := len(m.data); actual != rowSize {
+			t.Fatalf("rows mismatch.  got: %d", actual)
+		}
+
+		if actual := len(m.data[0]); actual != colSize {
+			t.Fatalf("col mismatch.  got: %d", actual)
+		}
+	})
+
+	t.Run("happy path: simple creation of a 2x5 matrix", func(t *testing.T) {
+		const (
+			rowSize = 2
+			colSize = 5
 		)
 
 		m, err := NewMatrix[int](rowSize, colSize)
@@ -41,6 +61,46 @@ func Test_NewMatrix(t *testing.T) {
 		}
 		if m != nil {
 			t.Fatalf("expect nil pointer on error")
+		}
+	})
+
+	t.Run("happy path: simple creation of a 2x5 uint matrix", func(t *testing.T) {
+		const (
+			rowSize = 2
+			colSize = 5
+		)
+
+		m, err := NewMatrix[uint](rowSize, colSize)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if actual := len(m.data); actual != rowSize {
+			t.Fatalf("rows mismatch.  got: %d", actual)
+		}
+
+		if actual := len(m.data[0]); actual != colSize {
+			t.Fatalf("col mismatch.  got: %d", actual)
+		}
+	})
+
+	t.Run("happy path: simple creation of a 2x5 uint matrix", func(t *testing.T) {
+		const (
+			rowSize = 2
+			colSize = 5
+		)
+
+		m, err := NewMatrix[float64](rowSize, colSize)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if actual := len(m.data); actual != rowSize {
+			t.Fatalf("rows mismatch.  got: %d", actual)
+		}
+
+		if actual := len(m.data[0]); actual != colSize {
+			t.Fatalf("col mismatch.  got: %d", actual)
 		}
 	})
 
