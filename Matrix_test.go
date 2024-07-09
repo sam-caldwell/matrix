71041d3dfdfa4faadc0f0a7ac63581f8bbe9617a
Sam Caldwell <github@samcaldwell.net>
2024-07-08 19:47:46 -0500

    add test for Matrix type

--- Matrix_test.go
+++ Matrix_test.go
@@ -5,12 +5,16 @@ import "testing"
 func TestMatrix_type(t *testing.T) {
 	t.Run("Matrix should be 2d matrix", func(t *testing.T) {
 		m := Matrix[int]{
-			{0, 1, 2, 3, 4},
-			{0, 1, 2, 3, 4},
+			data: [][]int{
+				{0, 1, 2, 3, 4},
+				{0, 1, 2, 3, 4},
+			},
 		}
-		if len(m) != 5 || len(m[0]) != 2 {
-			t.Fatalf("expect 2d matrix")
+		if len(m.data) != 2 {
+			t.Fatalf("expected 2 rows")
+		}
+		if len(m.data[0]) != 5 {
+			t.Fatalf("expected 5 cols")
 		}
 	})
-
 }
