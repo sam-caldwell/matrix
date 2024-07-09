12cf97d2853b9a81c2b0895896da3ef5e26365f4
Sam Caldwell <github@samcaldwell.net>
2024-07-08 19:51:01 -0500

    Add improvements to Matrix test

--- Matrix_test.go
+++ Matrix_test.go
@@ -17,4 +17,112 @@ func TestMatrix_type(t *testing.T) {
 			t.Fatalf("expected 5 cols")
 		}
 	})
+	t.Run("Matrix should support int", func(t *testing.T) {
+		_ = Matrix[int]{
+			data: [][]int{
+				{0, 1},
+				{0, 1},
+			},
+		}
+
+	})
+	t.Run("Matrix should support int8", func(t *testing.T) {
+		_ = Matrix[int8]{
+			data: [][]int8{
+				{0, 1},
+				{0, 1},
+			},
+		}
+
+	})
+	t.Run("Matrix should support int16", func(t *testing.T) {
+		_ = Matrix[int16]{
+			data: [][]int16{
+				{0, 1},
+				{0, 1},
+			},
+		}
+
+	})
+	t.Run("Matrix should support int32", func(t *testing.T) {
+		_ = Matrix[int32]{
+			data: [][]int32{
+				{0, 1},
+				{0, 1},
+			},
+		}
+
+	})
+	t.Run("Matrix should support int64", func(t *testing.T) {
+		_ = Matrix[int64]{
+			data: [][]int64{
+				{0, 1},
+				{0, 1},
+			},
+		}
+
+	})
+	t.Run("Matrix should support uint", func(t *testing.T) {
+		_ = Matrix[uint]{
+			data: [][]uint{
+				{0, 1},
+				{0, 1},
+			},
+		}
+
+	})
+	t.Run("Matrix should support uint8", func(t *testing.T) {
+		_ = Matrix[uint8]{
+			data: [][]uint8{
+				{0, 1},
+				{0, 1},
+			},
+		}
+
+	})
+	t.Run("Matrix should support uint16", func(t *testing.T) {
+		_ = Matrix[uint16]{
+			data: [][]uint16{
+				{0, 1},
+				{0, 1},
+			},
+		}
+
+	})
+	t.Run("Matrix should support uint32", func(t *testing.T) {
+		_ = Matrix[uint32]{
+			data: [][]uint32{
+				{0, 1},
+				{0, 1},
+			},
+		}
+
+	})
+	t.Run("Matrix should support uint64", func(t *testing.T) {
+		_ = Matrix[uint64]{
+			data: [][]uint64{
+				{0, 1},
+				{0, 1},
+			},
+		}
+
+	})
+	t.Run("Matrix should support float32", func(t *testing.T) {
+		_ = Matrix[float32]{
+			data: [][]float32{
+				{0.001, 0.002},
+				{0.003, 0.004},
+			},
+		}
+
+	})
+	t.Run("Matrix should support float64", func(t *testing.T) {
+		_ = Matrix[float64]{
+			data: [][]float64{
+				{0.001, 0.002},
+				{0.003, 0.004},
+			},
+		}
+
+	})
 }
