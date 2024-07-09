80d4a595f2e0dc7edd15ed4dd008631386ef20d3
Sam Caldwell <github@samcaldwell.net>
2024-07-08 20:57:30 -0500

    change panic() to returning error in Bitwise operations

--- Matrix.BitwiseNot.go
+++ Matrix.BitwiseNot.go
@@ -1,6 +1,9 @@
 package matrix
 
-import "github.com/sam-caldwell/errors"
+import (
+	"fmt"
+	"github.com/sam-caldwell/errors"
+)
 
 // Not - Perform a bitwise NOT operation on a Matrix
 //
@@ -10,32 +13,36 @@ func (lhs *Matrix[T]) Not() (result *Matrix[T], err error) {
 	lhs.lock.RLock()
 	defer lhs.lock.RUnlock()
 
+	if result, err = NewMatrix[T](lhs.rows(), lhs.cols()); err != nil {
+		return nil, err
+	}
+
 	for r := uint(0); r < lhs.rows(); r++ {
 
 		for c := uint(0); c < lhs.cols(); c++ {
-			switch v := any((*lhs).data[r][c]).(type) {
+			switch v := any((*lhs).data[c][r]).(type) {
 			case int:
-				result.data[r][c] = any(^v).(T)
+				result.data[c][r] = any(^v).(T)
 			case int8:
-				result.data[r][c] = any(^v).(T)
+				result.data[c][r] = any(^v).(T)
 			case int16:
-				result.data[r][c] = any(^v).(T)
+				result.data[c][r] = any(^v).(T)
 			case int32:
-				result.data[r][c] = any(^v).(T)
+				result.data[c][r] = any(^v).(T)
 			case int64:
-				result.data[r][c] = any(^v).(T)
+				result.data[c][r] = any(^v).(T)
 			case uint:
-				result.data[r][c] = any(^v).(T)
+				result.data[c][r] = any(^v).(T)
 			case uint8:
-				result.data[r][c] = any(^v).(T)
+				result.data[c][r] = any(^v).(T)
 			case uint16:
-				result.data[r][c] = any(^v).(T)
+				result.data[c][r] = any(^v).(T)
 			case uint32:
-				result.data[r][c] = any(^v).(T)
+				result.data[c][r] = any(^v).(T)
 			case uint64:
-				result.data[r][c] = any(^v).(T)
+				result.data[c][r] = any(^v).(T)
 			default:
-				panic(errors.UnsupportedType)
+				return nil, fmt.Errorf(errors.UnsupportedType)
 			}
 		}
 	}
